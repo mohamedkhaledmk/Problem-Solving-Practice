436b589e8dae3a18f70e566fa6b78b74c9d73583
Mohamed Khaled <mohamed181371@feng.bu.edu.eg>
2026-01-13 03:13:53 +0200

    ITI problem

--- ITI/Loops/tempCodeRunnerFile.cpp
+++ ITI/Loops/tempCodeRunnerFile.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
 int main()
 {
-    int n1, n2;
-    cin >> n1 >> n2;
-    int x, y, cnt = 0;
-    for (int i = 1; i <= 1000; i++)
+    int n;
+    cin >> n;
+    int arr[n][n] = {0};
+    int x, y;
+    for (int i = 0; i < n; i++)
     {
-        x = i * i;
-        y = n1 - x;
-        if ((y * y + i) == n2)
-            cnt++;
+        cin >> x >> y;
+        arr[x - 1][y - 1] = 1;
+        arr[y - 1][x - 1] = 1;
     }
-    cout << cnt;
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < n; j++)
+        {
+            sum += arr[i][j];
+        }
+        if (sum > 2)
+        {
+            cout << "WIN";
+            return 0;
+        }
+    }
+    cout << "FAIL";
     return 0;
 }
