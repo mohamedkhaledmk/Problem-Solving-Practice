0c5b67b3a8424fdfde84a315804661c71daebec0
Mohamed Khaled <mohamed181371@feng.bu.edu.eg>
2026-01-15 18:36:39 +0200

    temp code runner problem

--- ASSUIT/General Easy/tempCodeRunnerFile.cpp
+++ ASSUIT/General Easy/tempCodeRunnerFile.cpp
@@ -1,5 +1,46 @@
-if (i % 2 != 0)
-        {
-            c = '.';
-            f = !f;
-        }
+#include <iostream>
+#include <vector>
+using namespace std;
+#define ll long long
+
+ll fib(int x, vector<ll> &fibo)
+{
+    if (x <= 2)
+        return x - 1;
+    else if (fibo[x] == -1)
+    {
+        fibo[x] = fib(x - 1, fibo) + fib(x - 2, fibo);
+        return fibo[x];
+    }
+    else
+    {
+        return fibo[x];
+    }
+}
+bool isPrime(int n)
+{
+    if (n < 2)
+        return false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n;
+        cin >> n;
+        vector<ll> fibonnaci(n + 1, -1);
+        ll x = fib(n, fibonnaci);
+        cout << (isPrime(x) ? "prime" : "not prime") << endl;
+    }
+    return 0;
+}
