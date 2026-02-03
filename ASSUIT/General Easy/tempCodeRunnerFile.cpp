dc93c4938fcf905742e63937b12ba5a19374ea11
Mohamed Khaled <mohamed181371@feng.bu.edu.eg>
2026-02-03 21:50:57 +0200

    all done

--- ASSUIT/General Easy/tempCodeRunnerFile.cpp
+++ ASSUIT/General Easy/tempCodeRunnerFile.cpp
@@ -1,46 +1,34 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <algorithm>
 #define ll long long
-
-ll fib(int x, vector<ll> &fibo)
-{
-    if (x <= 2)
-        return x - 1;
-    else if (fibo[x] == -1)
-    {
-        fibo[x] = fib(x - 1, fibo) + fib(x - 2, fibo);
-        return fibo[x];
-    }
-    else
-    {
-        return fibo[x];
-    }
-}
-bool isPrime(int n)
-{
-    if (n < 2)
-        return false;
-    for (int i = 2; i * i <= n; i++)
-    {
-        if (n % i == 0)
-            return false;
-    }
-    return true;
-}
+using namespace std;
 
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
+    int n, x, num, cnt = 0;
+    cin >> n >> x;
+    for (int i = 0; i < n; i++)
     {
-
-        int n;
-        cin >> n;
-        vector<ll> fibonnaci(n + 1, -1);
-        ll x = fib(n, fibonnaci);
-        cout << (isPrime(x) ? "prime" : "not prime") << endl;
+        cin >> num;
+        bool arr[10]{};
+        bool flag = true;
+        while (num)
+        {
+            arr[num % 10] = true;
+            num /= 10;
+        }
+        for (int i = 0; i <= x; i++)
+        {
+            if (!arr[i])
+            {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+            cnt++;
     }
+    cout << cnt;
     return 0;
 }
