375b233044c5354574d5c132b155d7dfcffc1e5b
Mohamed Khaled <mohamed181371@feng.bu.edu.eg>
2026-02-14 23:40:10 +0200

    practice_a

---
+++ ACPC-ITI/Practice/Practice_A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    if ((x + y) % 2 != 0)
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
