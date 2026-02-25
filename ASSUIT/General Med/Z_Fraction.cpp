43e89bba24e68ed621cb4dff308eb51f5102c27b
Mohamed Khaled <mohamed181371@feng.bu.edu.eg>
2026-02-25 04:07:18 +0200

    Fraction prob

---
+++ ASSUIT/General Med/Z_Fraction.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+int main()
+{
+    int a1, b1, c, a2, b2;
+    cin >> a1 >> c >> b1 >> a2 >> c >> b2;
+
+    return 0;
+}
