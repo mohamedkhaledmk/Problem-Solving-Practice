af929673e1699049fe2eafa314507c4b1928dc86
mohamedkhaledmk <khaled25256@gmail.com>
2026-03-25 23:46:34 +0200

    theatre square

--- Random_CodeForces_Problems/A_Theatre_Square.cpp
+++ Random_CodeForces_Problems/A_Theatre_Square.cpp
@@ -3,9 +3,9 @@ using namespace std;
 #define ll long long
 int main()
 {
-    ll x,y,z;
-    cin>>x>>y>>z;
-    cout<<(x+z-1)/z * (y+z-1)/z;
-    
+    ll x, y, z;
+    cin >> x >> y >> z;
+    cout << ((x + z - 1) / z) * ((y + z - 1) / z);
+
     return 0;
 }
