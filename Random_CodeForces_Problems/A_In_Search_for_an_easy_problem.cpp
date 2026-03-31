6379467a2803a774908821668ea78f1e5881ff60
mohamedkhaledmk <khaled25256@gmail.com>
2026-04-01 00:09:20 +0200

    in search for an easy problem

---
+++ Random_CodeForces_Problems/A_In_Search_for_an_easy_problem.cpp
@@ -0,0 +1,19 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+int main(){
+    int n;cin>>n;int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+        if(arr[i])
+            {
+                cout<<"HARD";
+                return 0;
+            }
+    }
+    cout<<"EASY";
+    return 0;
+    
+    return 0;
+} // namespace stdint main()
