1af50793bf7a75aa9de2d635c5975bd730965192
mohamedkhaledmk <khaled25256@gmail.com>
2026-04-01 00:09:28 +0200

    saving for birthday

---
+++ ACPC-ITI/Placement/C_Saving_for_Bithday.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+    cin >> x >> y >> z;
+    if (x * y >= z)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+
+    return 0;
+}
