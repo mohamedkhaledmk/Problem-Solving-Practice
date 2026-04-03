4e9a9812b8ccd60cbefe061dfc65b458ac746481
mohamedkhaledmk <khaled25256@gmail.com>
2026-04-04 01:39:32 +0200

    domty distribution

---
+++ ACPC-ITI/Placement/F_Domty_distribution.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int d;
+    cin >> d;
+    if (d % 3 == 0)
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
