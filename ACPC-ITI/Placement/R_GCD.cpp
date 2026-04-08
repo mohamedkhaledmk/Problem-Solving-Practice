31b4656b884aba4c58241b1e9a3219c1067fddfc
mohamedkhaledmk <khaled25256@gmail.com>
2026-04-08 14:36:57 +0200

    GCD

---
+++ ACPC-ITI/Placement/R_GCD.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> vect(n);
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        bool flag = true;
+        int val;
+        cin >> val;
+        for (int i = 0; i < n; i++)
+        {
+            if (v[i] % val != 0)
+            {
+            }
+        }
+    }
+    return 0;
+}
