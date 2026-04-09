bf9ad9b795356983a1b99dbec54c156f2d00bbd1
mohamedkhaledmk <khaled25256@gmail.com>
2026-04-09 16:46:43 +0200

    welcome to school

---
+++ ACPC-ITI/Placement/U_Welcome_to_School.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int s, t;
+    cin >> s >> t;
+    if (s < t)
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
