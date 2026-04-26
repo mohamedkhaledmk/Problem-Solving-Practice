3d8f1495ad7a56b55d29bf92f4dd858cb468ec3f
mohamedkhaledmk <khaled25256@gmail.com>
2026-04-26 20:17:01 +0300

    marks

---
+++ ASSUIT/General Hard/F_Marks.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<string> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    int studs = 0;
+    for (int i = 0; i < m; i++)
+    {
+        bool flg = false;
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < m; k++)
+            {
+                        }
+        }
+        if (!flg)
+            studs++;
+    }
+    cout << studs;
+
+    return 0;
+}
