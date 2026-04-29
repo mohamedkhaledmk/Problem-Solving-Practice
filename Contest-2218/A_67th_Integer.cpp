6e92a207ab9173c3cfd99941083746ad91eb2329
mohamedkhaledmk <khaled25256@gmail.com>
2026-04-29 17:51:47 +0300

    67 int

---
+++ Contest-2218/A_67th_Integer.cpp
@@ -0,0 +1,18 @@
+#include <bits\stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    int n, x;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << "67" << endl;
+    }
+    return 0;
+}
